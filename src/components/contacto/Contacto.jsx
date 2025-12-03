c2e21bcc8e0c382010dd577888c00c847b085189
Gabriel <gabrielfernandez090997@gmail.com>
2025-12-03 18:10:10 -0300

    Se creó el componente Contacto con sus archivos jsx y css

---
+++ src/components/contacto/Contacto.jsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./Contacto.css";
+
+const Contacto = () => {
+  return (
+    <Container fluid className="border">
+      <Row>
+        <Col>Contacto</Col>
+        <Col> Formulario</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Contacto;

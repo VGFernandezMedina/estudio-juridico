878d483448cbf40197b3379fdf7974e5e3c297aa
Gabriel <gabrielfernandez090997@gmail.com>
2026-01-20 00:25:22 -0300

    Se agregó un encabezado a la sección de servicios con titulo y descripción

--- src/components/servicios/Servicios.jsx
+++ src/components/servicios/Servicios.jsx
@@ -2,11 +2,19 @@ import { Container, Row } from "react-bootstrap";
 import "./Servicios.css";
 import servicios from "../../data/servicesData.js";
 import ServiciosCard from "../serviciosCard/ServiciosCard";
+import { PiScales } from "react-icons/pi";
 
 const Servicios = () => {
   return (
     <Container fluid className="servicios pb-5">
-      <h2 className="py-3">Servicios</h2>
+      <section className="encabezado py-5">
+        <div className="d-flex justify-content-center align-items-center mb-3">
+          <PiScales size={20} />
+          <span>Servicios</span>
+        </div>
+        <h2>Acompañamiento legal claro, humano y profesional.</h2>
+        <p>Defiendo tus derechos con compromiso, cercanía y responsabilidad.</p>
+      </section>
 
       <Row className="row-servicios">
         {servicios.map((servicio) => (
@@ -15,6 +23,7 @@ const Servicios = () => {
             img={servicio.img}
             titulo={servicio.titulo}
             descripcion={servicio.descripcion}
+            etiqueta={servicio.etiqueta}
           />
         ))}
       </Row>
